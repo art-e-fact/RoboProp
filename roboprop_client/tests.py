f26e66c1c77305154cacc5f3960068a5833d219f
Tomo Norman <tomo.l.norman@gmail.com>
2023-06-07 17:06:03 +0900

    better naming on testing models

--- roboprop_client/tests.py
+++ roboprop_client/tests.py
@@ -3,8 +3,8 @@ from django.urls import reverse
 import unittest
 from unittest.mock import patch, MagicMock
 from roboprop_client.views import (
-    _get_assets,
-    _get_roboprop_asset_thumbnails,
+    _get_models,
+    _get_roboprop_model_thumbnails,
     mymodel_detail,
 )
 
@@ -15,26 +15,26 @@ mock_get.return_value is returned instead of making actual http request
 """
 
 
-class TestGetAssets(unittest.TestCase):
+class TestGetModels(unittest.TestCase):
     @patch("roboprop_client.views.requests.get")
-    def test_get_assets_roboprop(self, mock_get):
+    def test_get_models_roboprop(self, mock_get):
         mock_response = MagicMock()
-        mock_response.json.return_value = {"resource": ["asset1", "asset2"]}
+        mock_response.json.return_value = {"resource": ["model1", "model2"]}
         mock_get.return_value = mock_response
 
-        result = _get_assets("roboprop")
+        result = _get_models("roboprop")
 
-        self.assertEqual(result, ["asset1", "asset2"])
+        self.assertEqual(result, ["model1", "model2"])
 
 
-class TestGetRobopropAssetThumbnails(unittest.TestCase):
+class TestGetRobopropModelThumbnails(unittest.TestCase):
     @patch("roboprop_client.views.requests.get")
     def test_returns_list_of_dictionaries(self, mock_get):
         mock_response = MagicMock()
         mock_response.content = b"thumbnail"
         mock_get.return_value = mock_response
 
-        assets = [
+        models = [
             ".gitignore",
             "model1/thumbnails/1.png",
             "model2/thumbnails/2.png",
@@ -43,7 +43,7 @@ class TestGetRobopropAssetThumbnails(unittest.TestCase):
             "model2/5.png",
         ]
 
-        result = _get_roboprop_asset_thumbnails(assets)
+        result = _get_roboprop_model_thumbnails(models)
 
         self.assertIsInstance(result, list)
         self.assertIsInstance(result[0], dict)
@@ -57,7 +57,7 @@ class TestGetRobopropAssetThumbnails(unittest.TestCase):
         mock_response.content = b"thumbnail"
         mock_get.return_value = mock_response
 
-        assets = [
+        models = [
             ".gitignore",
             "model1/thumbnails/1.png",
             "model1/thumbnails/2.png",
@@ -66,7 +66,7 @@ class TestGetRobopropAssetThumbnails(unittest.TestCase):
             "model2/5.png",
         ]
 
-        result = _get_roboprop_asset_thumbnails(assets)
+        result = _get_roboprop_model_thumbnails(models)
 
         self.assertEqual(result[0]["name"], "model1")
         self.assertEqual(result[0]["image"], "dGh1bWJuYWls")
@@ -75,11 +75,9 @@ class TestGetRobopropAssetThumbnails(unittest.TestCase):
 
 
 class MyModelDetailTestCase(TestCase):
-    def test_model_name_matches_url(self):
+    def test_model_detail(self):
         model_name = "example_model"
         url = reverse("mymodel_detail", args=[model_name])
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
-
-        model = response.context["model"]
-        self.assertEqual(model["name"], model_name)
+        self.assertContains(response, model_name)

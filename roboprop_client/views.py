5b2a4fb31adefa9e609d976d31d2dc1b916d0020
Tomo Norman <tomo.l.norman@gmail.com>
2023-06-15 14:43:47 +0900

    display fuel models as well

--- roboprop_client/views.py
+++ roboprop_client/views.py
@@ -25,10 +25,10 @@ def _get_models(url):
     return models
 
 
-def _get_roboprop_model_thumbnails(models, gallery=True):
+def _get_roboprop_model_thumbnails(models, folder, gallery=True):
     thumbnails = []
     for model in models:
-        url = fileserver_url + "mymodels/" + model + "/thumbnails/"
+        url = fileserver_url + folder + model + "/thumbnails/"
         response = _make_get_request(url)
         if response.status_code == 200:
             thumbnail_data = response.json()["resource"]
@@ -36,7 +36,6 @@ def _get_roboprop_model_thumbnails(models, gallery=True):
                 # Just one thumbnail for each in mymodels.html
                 thumbnail_data = thumbnail_data[0:1]
             for data in thumbnail_data:
-                print("HELLO", data)
                 url = fileserver_url + data["path"] + "?is_base64=true"
                 response = _make_get_request(url)
                 thumbnail = base64.b64encode(response.content).decode("utf-8")
@@ -82,9 +81,10 @@ def home(request):
 
 def mymodels(request):
     roboprop_models = _get_models(fileserver_url + "mymodels/")
-    roboprop_model_thumbnails = _get_roboprop_model_thumbnails(roboprop_models)
-    fuel_thumbnails = []  # TODO:
-    gallery_thumbnails = roboprop_model_thumbnails + fuel_thumbnails
+    roboprop_model_thumbnails = _get_roboprop_model_thumbnails(roboprop_models, "mymodels/")
+    fuel_thumbnails = _get_models(fileserver_url + "fuelmodels/")
+    fuel_model_thumbnails = _get_roboprop_model_thumbnails(fuel_thumbnails, "fuelmodels/")
+    gallery_thumbnails = roboprop_model_thumbnails + fuel_model_thumbnails
 
     return render(request, "mymodels.html", {"thumbnails": gallery_thumbnails})
 
@@ -96,7 +96,6 @@ def mymodel_detail(request, model):
         # Convert a Django QueryDict to a dictionary
         model_config = dict(request.POST)
         model_config.pop("csrfmiddlewaretoken", None)
-        print(model_config)
         # Convert to xml before making out PUT request to update.
         model_config = _config_as_xml(model_config, "model")
         # Send an HTTP PUT request to update the model configuration

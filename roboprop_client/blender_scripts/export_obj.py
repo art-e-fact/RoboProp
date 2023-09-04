ec6b560fe3dbd9b1c7fbb3104b95db0735c39716
Polgár András <azazdeaz@gmail.com>
2023-09-04 17:08:04 +0200

    Oprimize meshes in export script

--- roboprop_client/blender_scripts/export_obj.py
+++ roboprop_client/blender_scripts/export_obj.py
@@ -15,6 +15,17 @@ else:
 args = parser.parse_known_args(argv)[0]
 output = Path(args.output)
 
+# Add Palanar Decimate modifyer to all meshes
+angle_limit = 12.0 # merge faces within this angle
+for obj in bpy.data.objects:
+    if obj.type == "MESH":
+        bpy.context.view_layer.objects.active = obj
+        bpy.ops.object.modifier_add(type="DECIMATE")
+        modifier = bpy.context.object.modifiers[-1]
+        modifier.decimate_type = "DISSOLVE"
+        modifier.angle_limit = angle_limit / 180.0 * 3.14159
+        bpy.ops.object.modifier_apply(modifier=modifier.name)
+
 bpy.ops.file.unpack_all(method="USE_LOCAL")
 
 bpy.ops.export_scene.obj(

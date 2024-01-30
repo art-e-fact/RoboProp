cf0c9e557167c2c819f4878c39a04a32918a2b01
Tomo Norman <tomo.l.norman@gmail.com>
2024-01-30 19:26:34 +0900

    Static files

--- roboprop/settings.py
+++ roboprop/settings.py
@@ -137,11 +137,21 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-COMPRESS_ROOT = BASE_DIR / "static"
+
+STATICFILES_DIRS = [
+    'static',
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+]
+
+COMPRESS_ROOT = STATIC_ROOT
 
 COMPRESS_ENABLED = True
-
-STATICFILES_FINDERS = ("compressor.finders.CompressorFinder",)
 
 LOGIN_URL = "/login"
 
